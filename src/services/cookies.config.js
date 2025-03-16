c543571eab2c783515190d68a577866b0053d99b
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-16 14:34:59 +0300

    Cookie module

--- src/services/cookies.config.js
+++ src/services/cookies.config.js
@@ -1,7 +1,9 @@
-export const cookie_config = {
+const cookie_config = {
   path: "/", // This is crucial - makes cookie available to entire domain
   httpOnly: false, // If you want the cookie to be inaccessible to JavaScript
   secure: process.env.NODE_ENV !== "development", // Use secure in production
   sameSite: "None",
   partitioned: true,
 };
+
+module.exports = [cookie_config];
