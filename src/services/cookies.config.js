68693eaadf8a5753c4372d34d490ed24dbdd124d
di.razafinjatovo <di.razafinjatovo@etechconsulting-mg.com>
2025-03-16 16:27:39 +0300

    Changement env

--- src/services/cookies.config.js
+++ src/services/cookies.config.js
@@ -1,7 +1,8 @@
 const cookie_config = {
   path: "/", // This is crucial - makes cookie available to entire domain
   httpOnly: false, // If you want the cookie to be inaccessible to JavaScript
-  secure: process.env.NODE_ENV !== "development", // Use secure in production
+  // secure: process.env.NODE_ENV !== "development", 
+  secure: true, 
   sameSite: "None",
   partitioned: true,
 };
